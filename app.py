085051563bd6a394938e1541d41ef3e277a41549
Denis <lazerman743@gmail.com>
2025-11-19 12:10:47 +0200

    Added a room route

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask,redirect,url_for,render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/<room>")
+def roomchat(room):
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
