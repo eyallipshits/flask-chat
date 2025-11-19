a1afabe11a1ac68c7ce3c758055a7d54df657d14
Denis <lazerman743@gmail.com>
2025-11-19 13:35:18 +0200

    Added POST method to the app.py

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
-from flask import Flask,redirect,url_for,render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
+rooms = {}
+
+
+# Go to the home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# go to a some room chat
 @app.route("/<room>")
 def roomchat(room):
     return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/api/chat/<room>", methods=["POST"])
+def api_chat_room(room):
+
+    if request.method == "POST":
+
+        # get from the user username
+        username = request.form["username"]
+
+        # get from the user the message
+        msg = request.form["msg"]
+
+        # if the room is not exists, create and store in dictionary
+        if room not in rooms:
+            rooms[room] = []
+
+            # if the room exists
+            if room in rooms:
+
+                # create a message list
+                messages = {"sender": username, "text": msg, "timestamp": datetime.now()}
+
+                # put that list to that specific room
+                rooms[room].append(messages)
+
+                return "Every thing is ok"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
