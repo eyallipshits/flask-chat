b7fdfbfdf2969eb44397bc982468dcc13067187f
Denis <lazerman743@gmail.com>
2025-11-19 14:58:29 +0200

    changed some code

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask,redirect,url_for,render_template, request, Response
+from datetime import datetime
 import os
 
 app = Flask(__name__)
 
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-
-@app.route('/')
-def home():
-    return render_template('index.html')
->>>>>>> origin
+#DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
 rooms = {}
-
 
 # Go to the home page
 @app.route("/")
@@ -24,33 +19,8 @@ def home():
 def roomchat(room):
     return render_template("index.html")
 
-@app.route("/api/chat/<room>", methods=["GET"])
+@app.route("/api/chat/<room>", methods=["GET","POST"])
 def get_chat(room):
-    room_file = os.path.join(DATA_DIR, f"{room}.txt")
-
-    if not os.path.exists(room_file):
-        return Response("No message yet.", mimetype="text/plain")
-    
-    lines = []
-    with open(room_file, "r", encoding="utf-8") as f:
-        for line in f:
-                line = line.strip()
-                if not line:
-                    continue
-
-                parts = [p.strip() for p in line.split("|", 2)]
-                if len(parts) != 3:
-                     continue
-                
-                timestamp, username, msg = parts
-                formatted = f"[{timestamp}] {username}: {msg}"
-                lines.append(formatted)
-
-    final_text = "\n".join(lines)
-    return Response(final_text, mimetype="text/plain")
-
-@app.route("/api/chat/<room>", methods=["POST"])
-def api_chat_room(room):
 
     if request.method == "POST":
 
@@ -64,17 +34,31 @@ def api_chat_room(room):
         if room not in rooms:
             rooms[room] = []
 
-            # if the room exists
-            if room in rooms:
+        # if the room exists
+        if room in rooms:
 
-                # create a message list
-                messages = {"sender": username, "text": msg, "timestamp": datetime.now()}
+            # create a message dictionary
+            messages = {"timestamp": datetime.now(),"sender": username, "text": msg}
 
-                # put that list to that specific room
-                rooms[room].append(messages)
+            # put that list to that specific room
+            rooms[room].append(messages)
+            
+            return "Every thing is ok"
 
-                return "Every thing is ok"
+    if request.method == "GET":
 
+        if room not in rooms :
+            return Response("No message yet.", mimetype="text/plain")
+            
+            #lines = []
+            output=[]
+        for mesg in rooms[room]:
+            
+                mesg['timestamp']
+                    
+        #final_text = "\n".join(rooms[room])
+        return Response(final_text, mimetype="text/plain")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
